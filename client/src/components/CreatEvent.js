bf6027ccfec119a1b03fecdc9f89f1d1f39f3d68
baltasar torres garcia <baltasartorresgarcia@192.168.1.132>
2019-04-01 01:17:43 +0200

    estilos nuevo evento

--- client/src/components/CreatEvent.js
+++ client/src/components/CreatEvent.js
@@ -72,17 +72,16 @@ export default class CreatEvent extends Component {
     
     render() {
         return (
-      <div>
+      <div className="container column-center">
           {/* <button onClick={this.openModal} className="btn newCoaster btn-primary">Nuevo evento</button> */}
             {/* <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal} style={customStyles}>  */}
             <NavBottom/>
        <NavTop/>
-                    <h2>Nuevo evento</h2>
+                    <h2 className="title">NUEVO EVENTO</h2>
                     <form onSubmit={this.handleSubmit}>
 
                         <div className="form-group">
                 
-                            <label>¿Distrito?</label>
                             <select type="options" className="form-control" name="distrito" value={this.state.location} onChange={e => this.handleChange(e)}>
                             {/* poner en las options los distritos de la api C.Madrid */}
                             <option>¿Distrito?</option>
@@ -90,18 +89,17 @@ export default class CreatEvent extends Component {
                         </div>
 
                         <div className="form-group">
-                            <label>Fecha</label>
-                            <input type="date" className="form-control" name="data" value={this.state.data} onChange={e => this.handleChange(e)} />
+                            
+                            <input type="date" className="form-control" name="data" placeholder="Fecha" value={this.state.data} onChange={e => this.handleChange(e)} />
                         </div>
 
                         <div className="form-group">
-                            <label>Hora</label>
-                            <input type="time" className="form-control" name="hour" value={this.state.hour} onChange={e => this.handleChange(e)}  />
+                            
+                            <input type="time" className="form-control" name="hour" placeholder="Hora" value={this.state.hour} onChange={e => this.handleChange(e)}  />
                         </div>
 
                         <div className="form-group">
-                            <label>Numero de participantes</label>
-                            <input type="number" className="form-control" name="participants" value={this.state.participants} onChange={e => this.handleChange(e)}  />
+                            <input type="number" className="form-control" placeholder="Numero de participantes" name="participants" value={this.state.participants} onChange={e => this.handleChange(e)}  />
                         </div>
 
                         {/* <div className="form-group">
