abd689570e661a60ff1a455b81edaa5106d31987
baltasar torres garcia <baltasartorresgarcia@mbp-de-amelia.home>
2019-03-31 22:39:30 +0200

    estilos formularios

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -48,35 +48,35 @@ export default class Signup extends Component {
 
   render() {
     return (
-      <div class="container">
-      <p>
+      <div class="container column-center">
+      {/* <p>
          ¿Estas registrado? 
          <Link to='/login' className="nav-element">
          Conectate</Link>
-      </p>
-      <Form onSubmit={this.handleFormSubmit}>
+      </p> */}
+      <p class="title">REGISTRATE</p>
+      <div class="signup">
+      <Form  onSubmit={this.handleFormSubmit}>
          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Username</Form.Label>
+            
             <Form.Control type="username" name="username" placeholder="Enter username" value={this.state.username} onChange={e =>
             this.handleChange(e)} />
-            <Form.Label>Email address</Form.Label>
+         
             <Form.Control type="email" name="email" placeholder="Enter email" value={this.state.email} onChange={e =>
             this.handleChange(e)} />
-            <Form.Text className="text-muted">
-               We'll never share your email with anyone else.
-            </Form.Text>
+            
          </Form.Group>
          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            
             <Form.Control type="password" name="password" placeholder="Password" value={this.state.password} onChange={e =>
             this.handleChange(e)} />
          </Form.Group>
          <div className="form-group">
-            <label>Imagen</label>
+           
             <input className="form-control" type="file" name="imageUrl" placeholder="image" value={this.state.imageUrl} onChange={e => this.handleChange(e)} />
          </div>
          <Button variant="primary" type="submit">Submit</Button>
-      </Form>
+      </Form></div>
    </div>
     )
   }
